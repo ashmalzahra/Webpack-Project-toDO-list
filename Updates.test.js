2fbd6f8c8e4cc936c0dee9b29dd894aefaa717ac
anderCM2 <ginoander_5@hotmail.com>
2023-01-27 14:20:15 -0500

    Edit and Delete tests added

--- Updates.test.js
+++ Updates.test.js
@@ -13,5 +13,12 @@ describe('TaskList', () => {
     task.complete(1, ulList);
     expect(document.getElementById('to-do-1').checked).toBe(true);
   });
-
+  test('Modify the description value of the input text', () => {
+    task.edit(1, ulList);
+    expect(document.getElementById('text-1').value).toBe('New text');
+  });
+  test('Delete the complete task on list, current : 2 => expected 1', () => {
+    task.clearAllCompleted(ulList);
+    expect(ulList.children).toHaveLength(1);
+  });
 });
