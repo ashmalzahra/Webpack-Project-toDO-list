d7fc932b4cc4853ca2c2f0a39ce0de1bb5ed8625
ashmalzahra <ashmal.zahraa@gmail.com>
2023-01-27 20:11:36 +0500

    Test file setup

---
+++ __mocks__/taskListDom.js
@@ -0,0 +1,18 @@
+const domContainer = () => {
+    document.body.innerHTML = `<div id="to-do">
+      <ul id="list">
+        <li id="task-0">
+          <input id='to-do-0' type='checkbox'></input>
+          <input id='text-0' type='text' value='this is a new'></input>
+        </li>
+        <li id="task-1">
+          <input id='to-do-1' type='checkbox'></input>
+          <input id='text-1' type='text' value='this is a second new'></input>
+        </li>
+      </ul>
+    </div>`;
+    const ulList = document.getElementById('list');
+    return ulList;
+  };
+  
+  module.exports = domContainer;
